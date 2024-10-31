4acbb7af17d82cdcdbc85dd4e8e75dc3c00ea1af
erhan <erhan@turuncuweb.net>
2024-10-31 14:24:17 +0200

    initialize

---
+++ turuncuweb-wordpress-core.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * TuruncuWeb Wordpress Core
+ *
+ * @package       TURUNCUWEB
+ * @author        Erhan KARACA
+ * @license       gplv2
+ * @version       1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:   TuruncuWeb Wordpress Core
+ * Plugin URI:    https://turuncuweb.net
+ * Description:   Wordpress core plugin for Turuncu Internet Solutions projects
+ * Version:       1.0.0
+ * Author:        Erhan KARACA
+ * Author URI:    https://turuncuweb.net
+ * Text Domain:   turuncuweb-wordpress-core
+ * Domain Path:   /languages
+ * License:       GPLv2
+ * License URI:   https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TuruncuWeb Wordpress Core. If not, see <https://www.gnu.org/licenses/gpl-2.0.html/>.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Include your custom code here.
